61af82636fcfaf4c302b59229e093957274ad25a
quatronitercio <quatronitercio@gmail.com>
2025-06-01 23:07:37 -0300

    Update Script_Funcionarios_Nao_Relacionados_a_Pedidos.sql

--- Script_Funcionarios_Nao_Relacionados_a_Pedidos.sql
+++ Script_Funcionarios_Nao_Relacionados_a_Pedidos.sql
@@ -1,4 +1,5 @@
 -- Listar os Funcionarios que não estejam relacionados a um Pedido.
+-- Feito a ligação entre as tabelas staffs (funcionarios) e orders (pedidos) listando o resultado nulo na tabela de funcionarios
 
 Select funcionarios.staff_id, 
        funcionarios.first_name, 
